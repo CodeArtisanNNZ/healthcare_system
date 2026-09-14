eaab3afa3b385af2bd12c98391348ef598d6554f
Nusaiba Nusrat Zaman <186475711+CodeArtisanNNZ@users.noreply.github.com>
2026-09-14 15:32:11 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -442,7 +442,7 @@ const config: NextConfig = {
         source: "/:path*",
         headers: [
           { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
         ],
       },
